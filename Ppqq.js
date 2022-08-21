66fa0dda1b965b31afff81193724b35c46e1754a
ahsinulumam <59972010+ahsinulumam@users.noreply.github.com>
2022-08-22 05:19:57 +0700

    Create Ppqq.js

---
+++ Ppqq.js
@@ -0,0 +1,92 @@
+      {"id":"paketTeleponAxis","paket":"Tlpn 30 menit All operator 7Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/AXT7","ket":"BELI"
+},
+      {"id":"paketTeleponAxis","paket":"Tlpn 100 menit All operator 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/AXT30","ket":"BELI"
+},
+      
+      {"id":"paketTeleponTelkomsel","paket":"Cek 400Menit 3Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/CEKSTB400","ket":"BELI"
+},
+      {"id":"paketTeleponTelkomsel","paket":"Cek 600Menit 7Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/CEKSTB600","ket":"BELI"
+},
+      {"id":"paketTeleponTelkomsel","paket":"Cek 1300Menit 15Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/CEKSTB1300","ket":"BELI"
+},
+      {"id":"paketTeleponTelkomsel","paket":"Cek 2100Menit 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/CEKSTB2100","ket":"BELI"
+},
+      {"id":"paketTeleponTelkomsel","paket":"Bayar 400Menit 3Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/BYRSTB400","ket":"BELI"
+},
+      {"id":"paketTeleponTelkomsel","paket":"Bayar 600Menit 7Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/BYRSTB600","ket":"BELI"
+},
+      {"id":"paketTeleponTelkomsel","paket":"Bayar 2100Menit 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/BYRSTB2100","ket":"BELI"
+},
+      {"id":"paketTeleponTelkomsel","paket":"Bayar Telkomsel Nelepon Sepuasnya","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/BYRTNS","ket":"BELI"
+},
+      {"id":"paketTeleponTelkomsel","paket":"Bayar Telkomsel Nelepon Bulk","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/BYRTNB","ket":"BELI"
+},
+      
+      {"id":"paketTeleponThree","paket":"20 Menit All operator 7Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/TN5","ket":"BELI"
+},
+      {"id":"paketTeleponThree","paket":"375 Menit sesama 7Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/TN2","ket":"BELI"
+},
+      {"id":"paketTeleponThree","paket":"50Menit All operator 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/TN3","ket":"BELI"
+},
+      {"id":"paketTeleponThree","paket":"60Menit All operator 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/TN15","ket":"BELI"
+},
+      {"id":"paketTeleponThree","paket":"800Menit ke sesama 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/TN4","ket":"BELI"
+},
+      {"id":"paketTeleponThree","paket":"150Menit All operator 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/TN30","ket":"BELI"
+},
+      {"id":"paketTeleponThree","paket":"250Menit All operator 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/TN6","ket":"BELI"
+},
+      
+      {"id":"paketTeleponIndosat","paket":"1000Menit sesama 1Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/IN1","ket":"BELI"
+},
+      {"id":"paketTeleponIndosat","paket":"Unlimited sesama 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/IN20","ket":"BELI"
+},
+      {"id":"paketTeleponIndosat","paket":"unlimited sesama + 50Menit all 7Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/IN12","ket":"BELI"
+},
+      {"id":"paketTeleponIndosat","paket":"unlimited sesama + 30Menit all 14Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/IN2","ket":"BELI"
+},
+    {"id":"paketTeleponIndosat","paket":"unlimited sesama + 60Menit all 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/IN6","ket":"BELI"
+},
+      {"id":"paketTeleponIndosat","paket":"unlimited sesama + 250Menit all 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/IN50","ket":"BELI"
+},
+      
+     {"id":"paketTeleponXl","paket":"300Menit + 50Menit all 7Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/XN12","ket":"BELI"
+},
+     {"id":"paketTeleponXl","paket":"200Menit sesama 14Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/XN15","ket":"BELI"
+},
+     {"id":"paketTeleponXl","paket":"500Menit sesama 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/XN35","ket":"BELI"
+},
+     {"id":"paketTeleponXl","paket":"200Menit + 400Sms sesama XL 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/XNS35","ket":"BELI"
+},
+     {"id":"paketTeleponXl","paket":"250Menit All operator 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/XN70","ket":"BELI"
+},
+     {"id":"paketTeleponXl","paket":"300Menit All operator 30Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/XN75","ket":"BELI"
+},
+     {"id":"paketTeleponXl","paket":"500Menit All operator 90Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/XN7","ket":"BELI"
+},
+     
+     
+     {"id":"paketSmsIndosat","paket":"Sms 300 kesesama & 100 kesemua","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/ISM5","ket":"BELI"
+},
+    {"id":"paketSmsIndosat","paket":"Sms 600 kesesama & 200 kesemua","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/ISM10","ket":"BELI"
+},
+   {"id":"paketSmsIndosat","paket":"Sms 2000 kesesama & 500 kesemua","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/ISM25","ket":"BELI"
+},
+   
+     {"id":"paketSmsTelkomsel","paket":"All operator 200sms 1Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/SS1","ket":"BELI"
+},
+     {"id":"paketSmsTelkomsel","paket":"All operator 75sms 1Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/SS5","ket":"BELI"
+},
+     {"id":"paketSmsTelkomsel","paket":"All operator 250sms 1Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/SS2","ket":"BELI"
+},
+     {"id":"paketSmsTelkomsel","paket":"All operator 500sms 7Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/SS500","ket":"BELI"
+},
+     {"id":"paketSmsTelkomsel","paket":"All operator 1000sms 15Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/SS1000","ket":"BELI"
+},
+ 
+     {"id":"paketSmsThree","paket":"All operator 250sms 7Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/TS250","ket":"BELI"
+},
+     {"id":"paketSmsThree","paket":"All operator 600sms 14Hari","harga":"Rp6.065","link":"https://pulsaku.id/widget/pulsa/jj/0505D0/TS600","ket":"BELI"
+},
+ 
+      
